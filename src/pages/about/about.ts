7ec9dc7ce694145ffd570b3262ebd514ccdd61c1
tojo <aud.jeanba@gmail.com>
2019-10-07 12:10:42 +0300

    about change

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
 
 @Component({
-  selector: "page-map",
-  templateUrl: "map.html"
+  selector: "page-about",
+  templateUrl: "about.html"
 })
 export class AboutPage {
   constructor(public navCtrl: NavController) {}
